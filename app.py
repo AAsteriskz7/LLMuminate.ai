72dbf75ce51aa8c1e136610bb412eae8c59243a1
Lucas Kang <53574972+lkang172@users.noreply.github.com>
2024-10-26 14:46:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from extract_keywords import extract_k
 from extract_quotes import extract_q
+import json
 
 app = Flask(__name__)
 llm_message = ""
@@ -15,11 +16,12 @@ def get_data():
     llm_message = llm_message['message']
     
     keywords = extract_k(llm_message)
-    #print(f'keywords: {keywords}')
     fact_check = extract_q(keywords, llm_message)
 
-    print(fact_check)
-    return jsonify({"status": "Message received!"}), 201
+    if isinstance(fact_check, str):
+        fact_check = json.loads(fact_check)
+
+    return jsonify(fact_check), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
